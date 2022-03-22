75fe11b62ed8ff3077502c57c7d796f03de7adae
henryaugusta <henryaugusta4@gmail.com>
2022-03-23 05:57:46 +0700

    add method in base activity

--- app/src/main/java/com/feylabs/halalkan/utils/base/BaseActivity.kt
+++ app/src/main/java/com/feylabs/halalkan/utils/base/BaseActivity.kt
@@ -1,5 +1,6 @@
 package com.feylabs.halalkan.utils.base
 
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,6 +11,22 @@ open class BaseActivity : AppCompatActivity() {
         if (!isLong)
             duration = Toast.LENGTH_SHORT
         Toast.makeText(this, text, duration).show()
+    }
+
+    fun View.makeVisible() {
+        viewVisible(this)
+    }
+
+    fun View.makeGone() {
+        viewGone(this)
+    }
+
+    fun viewGone(view: View) {
+        view.visibility = View.GONE
+    }
+
+    fun viewVisible(view: View) {
+        view.visibility = View.VISIBLE
     }
 
 }
