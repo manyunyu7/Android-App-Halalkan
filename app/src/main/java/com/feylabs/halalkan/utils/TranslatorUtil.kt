17fe03a5a12cb799d3aefb0c04da867b649d2926
manyunyu7 <henryaugusta4@gmail.com>
2022-05-22 14:28:13 +0700

    add language flag mapper

--- app/src/main/java/com/feylabs/halalkan/utils/TranslatorUtil.kt
+++ app/src/main/java/com/feylabs/halalkan/utils/TranslatorUtil.kt
@@ -6,15 +6,25 @@ object TranslatorUtil {
     val flagMap = mutableMapOf<String, String>()
 
 
-    fun setup(){
+    fun setup() {
         //https://flagcdn.com/en/codes.json
         flagMap["ko"] = "kr"
         flagMap["id"] = "id"
         flagMap["id"] = "id"
         flagMap["sq"] = "al"
+        flagMap["en"] = "gb" //england
+        flagMap["ms"] = "my" //malaysia
+        flagMap["ja"] = "jp" //japan
+        flagMap["sa"] = "ar" //arabic
+        flagMap["fr"] = "fr" //prancis
+        flagMap["de"] = "fr" //german
+        flagMap["tr"] = "tr" //turkey
+        flagMap["zh-CN"] = "cn" //chinese simplified
+        flagMap["zh-TW"] = "cn" //chinese traditional
+        flagMap["hi"] = "in" //Hindustan a.k.a India
     }
 
-    fun getCountryFlagCodeFromGoogle(code:String) :String{
+    fun getCountryFlagCodeFromGoogle(code: String): String {
         setup()
         return flagMap[code] ?: code
     }
@@ -50,7 +60,34 @@ object TranslatorUtil {
             ),
             SearchWithImageModel(
                 0, "Vietnamese", "", "vi"
-            )
+            ),
+            SearchWithImageModel(
+                0, "Japan", "", "ja"
+            ),
+            SearchWithImageModel(
+                0, "Portuguese", "", "pt"
+            ),
+            SearchWithImageModel(
+                0, "Romanian", "", "ro"
+            ),
+            SearchWithImageModel(
+                0, "Arabic", "", "ar"
+            ),
+            SearchWithImageModel(
+                0, "Prancis", "", "fr"
+            ),
+            SearchWithImageModel(
+                0, "Chinese Simplified", "", "zh-CN"
+            ),
+            SearchWithImageModel(
+                0, "Chinese Traditional", "", "zh-CN"
+            ),
+            SearchWithImageModel(
+                0, "Turkish", "", "tr"
+            ),
+            SearchWithImageModel(
+                0, "Hindi", "", "hi"
+            ),
         )
 
 }
