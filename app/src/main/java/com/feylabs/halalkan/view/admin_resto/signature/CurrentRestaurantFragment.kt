de73013cd362674298e68d43de52073410f11f99
Henry Augusta <henryaugusta4@gmail.com>
2022-08-17 20:33:09 +0700

    (core,resto): resto  -> create update phone, and certification, and resto type

--- app/src/main/java/com/feylabs/halalkan/view/admin_resto/signature/CurrentRestaurantFragment.kt
+++ app/src/main/java/com/feylabs/halalkan/view/admin_resto/signature/CurrentRestaurantFragment.kt
@@ -1,6 +1,7 @@
 package com.feylabs.halalkan.view.admin_resto.signature
 
 import android.Manifest
+import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
@@ -13,6 +14,8 @@ import android.widget.ArrayAdapter
 import androidx.core.content.ContextCompat
 import androidx.core.widget.addTextChangedListener
 import androidx.navigation.fragment.findNavController
+import com.adevinta.leku.*
+import com.feylabs.halalkan.MainViewModel
 import com.feylabs.halalkan.R
 import com.feylabs.halalkan.customview.RazkyGalleryActivity
 import com.feylabs.halalkan.customview.imagepreviewcontainer.CustomViewPhotoModel
@@ -29,6 +32,7 @@ import com.feylabs.halalkan.utils.snackbar.SnackbarType
 import com.feylabs.halalkan.view.admin_resto.AdminRestoViewModel
 import com.tangxiaolv.telegramgallery.GalleryActivity
 import com.tangxiaolv.telegramgallery.GalleryConfig
+import org.koin.android.viewmodel.ext.android.sharedViewModel
 import org.koin.android.viewmodel.ext.android.viewModel
 import timber.log.Timber
 import java.io.File
@@ -42,7 +46,9 @@ class CurrentRestaurantFragment : BaseFragment() {
     private var _binding: FragmentXrestoCurrentBinding? = null
     private val binding get() = _binding!!
 
+    val mainViewModel: MainViewModel by sharedViewModel()
     val viewModel by viewModel<AdminRestoViewModel>()
+
     private val PERMISSION_CODE_STORAGE = 1001
 
     private var mapCert = mutableMapOf<String, String>()
@@ -53,7 +59,7 @@ class CurrentRestaurantFragment : BaseFragment() {
 
     override fun initUI() {
         arguments?.getString("name")?.let {
-            binding.pageTitle.text=it
+            binding.pageTitle.text = it
         }
     }
 
@@ -98,16 +104,45 @@ class CurrentRestaurantFragment : BaseFragment() {
             findNavController().navigate(R.id.navigation_manageFoodFragment)
         }
 
+        binding.labelInfoResto.setOnClickListener {
+            val lat = mainViewModel.liveLatitude.value ?: -99
+            val long = mainViewModel.liveLongitude.value ?: -99
+
+            val locationPickerIntent = LocationPickerActivity.Builder()
+                .withLocation(lat.toDouble(), long.toDouble())
+                .withDefaultLocaleSearchZone()
+                .shouldReturnOkOnBackPressed()
+                .withStreetHidden()
+                .withCityHidden()
+                .withZipCodeHidden()
+                .withSatelliteViewHidden()
+                .withGoogleTimeZoneEnabled()
+                .withVoiceSearchHidden()
+                .withUnnamedRoadHidden()
+                .build(requireContext())
+
+            startActivityForResult(locationPickerIntent, 212)
+        }
+
         setupMenuAction()
     }
 
     private fun setupMenuAction() {
         binding.apply {
-            menuAddress.build("Address",getMuskoDrawable(R.drawable.ic_menu_resto_address))
-            menuRestoPhone.build("Phone",getMuskoDrawable(R.drawable.ic_menu_resto_phone))
-            menuOperatingHour.build("Operating Hour",getMuskoDrawable(R.drawable.ic_menu_resto_operating_hours))
-            menuTypeResto.build("Type of Restaurant",getMuskoDrawable(R.drawable.ic_menu_resto_type))
-            menuCertification.build("Certification",getMuskoDrawable(R.drawable.ic_menu_resto_certified))
+            menuAddress.build("Address", getMuskoDrawable(R.drawable.ic_menu_resto_address))
+            menuRestoPhone.build("Phone", getMuskoDrawable(R.drawable.ic_menu_resto_phone))
+            menuOperatingHour.build(
+                "Operating Hour",
+                getMuskoDrawable(R.drawable.ic_menu_resto_operating_hours)
+            )
+            menuTypeResto.build(
+                "Type of Restaurant",
+                getMuskoDrawable(R.drawable.ic_menu_resto_type)
+            )
+            menuCertification.build(
+                "Certification",
+                getMuskoDrawable(R.drawable.ic_menu_resto_certified)
+            )
 
             menuCertification.setOnClickListener {
                 findNavController().navigate(R.id.navigation_editRestoCertificationFragment)
@@ -152,6 +187,21 @@ class CurrentRestaurantFragment : BaseFragment() {
     }
 
     private fun setupRestoData(data: AllRestoNoPagination?) {
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK && data != null) {
+            if (requestCode == 212) {
+                val latitude = data.getDoubleExtra(LATITUDE, 0.0)
+                val longitude = data.getDoubleExtra(LONGITUDE, 0.0)
+                val address = data.getStringExtra(LOCATION_ADDRESS)
+                val lekuPoi = data.getParcelableExtra<LekuPoi>(LEKU_POI)
+
+                showSnackbar("$latitude $longitude $address")
+            }
+        }
+        if (resultCode == Activity.RESULT_CANCELED) {
+        }
     }
 
 
