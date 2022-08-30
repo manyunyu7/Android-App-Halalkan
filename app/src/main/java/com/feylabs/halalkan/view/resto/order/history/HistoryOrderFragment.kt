24535f1c8b735ebf24fb0be85d5dca0a292daea1
Henry Augusta <henryaugusta4@gmail.com>
2022-08-30 11:25:43 +0700

    (driver): order  -> fix history order fragment

---
+++ app/src/main/java/com/feylabs/halalkan/view/resto/order/history/HistoryOrderFragment.kt
@@ -0,0 +1,129 @@
+package com.feylabs.halalkan.view.resto.order.history
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.feylabs.halalkan.R
+import com.feylabs.halalkan.data.remote.QumparanResource
+import com.feylabs.halalkan.data.remote.reqres.order.history.OrderHistoryModel
+import com.feylabs.halalkan.databinding.FragmentHistoryOrderBinding
+import com.feylabs.halalkan.databinding.FragmentListAndSearchPrayerRoomBinding
+import com.feylabs.halalkan.utils.CommonUtil.makeGone
+import com.feylabs.halalkan.utils.base.BaseFragment
+import com.feylabs.halalkan.utils.snackbar.SnackbarType
+import com.feylabs.halalkan.view.resto.OrderViewModel
+import com.feylabs.halalkan.view.resto.main.FoodAdapterType
+import com.feylabs.halalkan.view.resto.main.RestoFoodAdapter
+import org.koin.android.viewmodel.ext.android.viewModel
+
+
+class HistoryOrderFragment : BaseFragment() {
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private var _binding: FragmentHistoryOrderBinding? = null
+    private val binding get() = _binding!!
+
+    val viewModel: OrderViewModel by viewModel()
+
+    private var currentPage = 1
+
+    private val mAdapter by lazy { HistoryOrderAdapter() }
+
+    override fun initUI() {
+        setupBottomNav()
+        binding.btnBack.setOnClickListener {
+            findNavController().navigateUp()
+        }
+
+        binding.rv.apply {
+            adapter = mAdapter
+            layoutManager = setLayoutManagerLinear()
+        }
+
+        mAdapter.setupAdapterInterface(object : HistoryOrderAdapter.ItemInterface {
+            override fun onclick(model: OrderHistoryModel) {
+                loadNextPage()
+            }
+        })
+
+    }
+
+    private fun loadNextPage() {
+        val currentPage = mAdapter.page
+        if (currentPage >= currentPage)
+            showSnackbar("You are on the last page")
+        else
+            viewModel.getRestoHistory(page = currentPage + 1, restoId = getChoosenResto())
+    }
+
+
+    private fun setupBottomNav() {
+        binding.bottomNav.apply {
+            setBottomMenuActive(tvMenuHistory)
+            setBottomMenuActive(ivMenuHistory)
+            btnMenuRestoHome.setOnClickListener {
+                findNavController().popBackStack(R.id.navigation_restoMainFragment, false)
+            }
+            btnMenuOrderHistory.setOnClickListener {
+            }
+        }
+    }
+
+
+    override fun initObserver() {
+        viewModel.orderHistoryLiveData.observe(viewLifecycleOwner) {
+            when (it) {
+                is QumparanResource.Default -> {
+                    showLoading(false)
+                }
+                is QumparanResource.Error -> {
+                    showLoading(false)
+                    showSnackbar(it.message.toString(), SnackbarType.ERROR)
+                }
+                is QumparanResource.Loading -> {
+                    showLoading(true)
+                }
+                is QumparanResource.Success -> {
+                    showLoading(false)
+                    it.data?.orderData?.let {
+                        mAdapter.setWithNewData(it.toMutableList())
+                        mAdapter.notifyDataSetChanged()
+                    }
+                }
+            }
+        }
+
+    }
+
+    private fun showLoading(b: Boolean) {
+        if (b) {
+            binding.loadingAnim.makeVisible()
+        } else {
+            binding.loadingAnim.makeGone()
+        }
+    }
+
+    override fun initAction() {
+    }
+
+    override fun initData() {
+        viewModel.getOrderHistory()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHistoryOrderBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
